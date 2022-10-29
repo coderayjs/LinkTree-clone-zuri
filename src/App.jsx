d64ee2c578c456762321146174c88192e161ead4
Satoshi6000 <livetalk001@gmail.com>
2022-10-29 17:53:47 +0100

    header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Navbar from './Navbar'
+
 
 function App() {
 return (
-  <Navbar/>
+  
 )
 }
 
