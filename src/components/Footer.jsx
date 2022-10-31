5183239595be2a7e360510109603da8e99a67e3e
Satoshi6000 <livetalk001@gmail.com>
2022-10-31 03:54:48 +0100

    updated and ready to deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,15 +6,19 @@ import zuriimpress from "../img/zuri-ingressive-img.png";
 
 const Footer = () => {
 return(
-  <div className="footer">
-    <div className="footer-container">
+  <div className="footer max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+    <div className="footer-container md:flex">
+
     </div>
       <div className="footer-content">
       </div>
       <div className="footer__logo">
           <img src={zuri} alt="zuri" className="footer__logo" />
+          <p className="footer-text">
+          HNG Internship 9 Frontend Task
+          <img src={zuriimpress} alt="zuriimpress" className="zuriimpress"/>
+          </p>
         </div>
-        
 
   </div>
 
