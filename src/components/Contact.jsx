aaa8e95aa395cd30dfa147a99f5ed4e0b40666db
Satoshi6000 <livetalk001@gmail.com>
2022-11-05 04:53:04 +0100

    contact Me componet

---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+
+const Contact = () => {
+    return (
+        <div className="Contact Me">
+            <h1>Contact Me</h1>
+    );
+};
