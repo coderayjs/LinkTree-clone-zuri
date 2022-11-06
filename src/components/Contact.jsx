b6424379fa45e4a7d2883aeff96f1ebdc5968c91
Satoshi6000 <livetalk001@gmail.com>
2022-11-06 06:23:22 +0100

    contact form updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { CheckBox } from "@mui/icons-material";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -6,9 +7,32 @@ const Contact = () => {
     return (
         <div className="contactMe">
             <div className="contact__content"/> 
-               <h1>Contact Me</h1>
+               <h1 className="heading">Contact Me</h1>
                 <p>Feel free to contact me on any of the platforms below</p>
-            <div className="">
+            <div className="form">
+                <form>
+                    <div className="form__group"/>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="fname" id="fname" placeholder="First Name" />
+                        <label htmlFor="name">Last Name</label>
+                        <input type="text" name="lname" id="lname" placeholder="Last Name" />
+                        <div className="email">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" id="email" placeholder="Email" />
+                        </div>
+                        <div className="message">
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                        </div>
+                        <div className="Checkbox">
+                        <input type="checkbox" name="checkbox" id="checkbox" />
+                        <label htmlFor="checkbox">You agree to providing your data to {name} who may contact you.</label>
+
+                        </div>
+                        <div className="sendMessage">
+                            <input type="submit" value="Send Message" />
+                        </div>
+                </form>
 
             </div>
         </div>
